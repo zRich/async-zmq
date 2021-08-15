08f8715a2725149a4b835a181280cb441224d8ad
zRich <zhao.zhenhua@gmail.com>
2021-08-15 11:32:41 +0800

    type define

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,46 @@
+mod message;
+
+pub use message::*;
+
+
 #[allow(unused_imports)]
-use zmq;
+use zmq::{Context, Error, Socket, Socket, SocketEvent, SocketType};
 
-pub enum ZmqSocketType {
-    PAIR,
-    PUB,
-    SUB,
-    REQ,
-    REP,
-    DEALER,
-    ROUTER,
-    PULL,
-    PUSH,
-    XPUB,
-    XSUB,
-    STREAM,
+use futures;
+
+type ZmqSocketType = SocketType;
+// pub enum ZmqSocketType {
+//     PAIR,
+//     PUB,
+//     SUB,
+//     REQ,
+//     REP,
+//     DEALER,
+//     ROUTER,
+//     PULL,
+//     PUSH,
+//     XPUB,
+//     XSUB,
+//     STREAM,
+// }
+
+type ZmqError = zmq::Error;
+
+type ZmqResult = Result<T, ZmqError>;
+
+pub struct ZmqContext {
+    ctx: zmq::Context,
 }
-
-pub struct ZmqContext;
 
 impl ZmqContext {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            ctx: zmq::Context::new(),
+        }
+    }
+
+    pub fn Socket(&self, socket_type: ZmqSocketType) -> Result<ZmqSocket> {
+        self.Socket(socket_type)
     }
 }
 
@@ -34,3 +54,15 @@ impl ZmqSocket {
         Self { ctx, socket_type }
     }
 }
+
+pub trait ZmqEvent {
+}
+
+
+pub trait ZmqSend {
+    async fn send(&mut self, message: ZmqMessage) -> ZmqResult<()>;
+ }
+
+ pub trait ZmqRecv {
+    async fn recv(&mut self) -> ZmqResult<ZmqMessage>;
+ }
