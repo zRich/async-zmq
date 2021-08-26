0331baeb174eae3dd68f8aafc6721df7a16edd0c
ZHAO Zhenhua <zhao.zhenhua@qq.com>
2021-08-26 19:42:04 +0800

    import from libzmq

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/libzmq.rs"));
+
 #![allow(trivial_numeric_casts)]
 mod error;
 mod message;
